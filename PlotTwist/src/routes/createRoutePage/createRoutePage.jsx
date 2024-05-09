eabb5190c252f05e493159f2318a0910a591211d
arseniyus <r.kozachenko2000@gmail.com>
2024-05-09 10:34:09 +0100

    draft skeleton create route panel

--- PlotTwist/src/routes/createRoutePage/createRoutePage.jsx
+++ PlotTwist/src/routes/createRoutePage/createRoutePage.jsx
@@ -1,7 +1,27 @@
 export default function CreateRoutePage() {
   return (
-    <div>
-      <h1>Create Route Page</h1>
-    </div>
+    <>
+      <header className="fixed-header">
+        <h1>Fixed Header</h1>
+      </header>
+      <body>
+        {/* p tag below header */}
+        {/* map component */}
+        <section className="createRoute__panel">
+          <div className="createRoute__panelETABar">
+            <ol className="">
+              <li className="createRoute__panelETABar__startPoint"></li>
+              <li className="createRoute__panelETABar__stops"></li>
+              <li className="createRoute__panelETABar__endPoint"></li>
+            </ol>
+          </div>
+          <div className="createRoute__panel__actionButtons">
+            <button></button>
+            <button></button>
+            <button></button>
+          </div>
+        </section>
+      </body>
+    </>
   );
 }
