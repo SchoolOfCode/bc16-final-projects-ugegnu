fc727832146d13b52c4c578d06eb8fe0a6579aff
Stacie Tilsley <stacietilsley98@gmail.com>
2024-05-16 23:17:40 +0100

    updated homePage with animation, pls dont hurt me Jaque

--- PlotTwist/src/routes/homePage/homePage.jsx
+++ PlotTwist/src/routes/homePage/homePage.jsx
@@ -8,28 +8,32 @@ export default function HomePage() {
   return (
     <>
       <main className="mainHomePage">
-        <img
-          src={HomePagecoachlayer}
-          alt="spinningWorld"
-          className="mainHomePage__HomePagecoachlayer"
-        />
-        <img
-          src={HomePageworldlayer}
-          alt="spinningWorld"
-          className="mainHomePage__HomepageWorldLayer"
-        />
-        <img className="mainHomePage__logo" src={FullLogo}></img>
-        <div className="mainHomePage__routes">
-          <Link to={"create-route"}>
-            <button className="mainHomePage__createRouteButtons">
-              Create Routes
-            </button>
-          </Link>
-          <Link to={"saved-routes"}>
-            <button className="mainHomePage__savedRoutesButtons">
-              Saved Routes
-            </button>
-          </Link>
+        <div className="layers">
+          <img
+            src={coachLayer}
+            alt="spinningWorld"
+            className="mainHomePage__HomePageCoachlayer"
+          />
+          <img
+            src={HomePageworldlayer}
+            alt="spinningWorld"
+            className="mainHomePage__HomepageWorldLayer"
+          />
+        </div>
+        <div className="overLayers">
+          <img className="mainHomePage__logo" src={FullLogo}></img>
+          <div className="mainHomePage__routes">
+            <Link to={"create-route"}>
+              <button className="mainHomePage__createRouteButtons">
+                Create Routes
+              </button>
+            </Link>
+            <Link to={"saved-routes"}>
+              <button className="mainHomePage__savedRoutesButtons">
+                Saved Routes
+              </button>
+            </Link>
+          </div>
         </div>
       </main>
     </>
