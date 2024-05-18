13caa0121f373adef07baec3764e8bdcb8cc9b52
JakFoster <jakfoster97@outlook.com>
2024-05-18 15:27:29 +0100

    Tidied up directions data css

--- PlotTwist/src/components/directionsData/directionsData.jsx
+++ PlotTwist/src/components/directionsData/directionsData.jsx
@@ -216,89 +216,7 @@ export default function DirectionsData({
     <>
       {routeIsCreated && directionsResult ? (
         <>
-          {/* this && will make sure that when isPopUPOPen is true, it will render the div and all the things in it... we can change whether or not is popup is true/false by clicking on the save route or the X button as these both have the function handlePopUp (this is waaay above and basically will change the isPopUp to be true/false (the opposite of what it currently is)...*/}
-          {isPopUpOpen && (
-            <div className="routeDate__popupContainer">
-              <div className="routeData__popup">
-                <button
-                  className="routeData__popup__closingButton"
-                  onClick={handlePopUp}
-                >
-                  X
-                </button>
-                <form className="routeData__form" onSubmit={handleSubmit}>
-                  <label
-                    className="routeData__label"
-                    htmlFor="routeName"
-                  ></label>
-                  <input
-                    className="routeData__nameInput"
-                    type="text"
-                    id="routeName"
-                    name="routeName"
-                    onChange={handleInputChange}
-                    value={routeName}
-                    placeholder="Enter route name..."
-                    required
-                  />
-                  <button className="routeData__saveRouteButton">Save</button>
-                </form>
-                {isSuccess && (
-                  <div>
-                    <img
-                      src={savedRouteConfirmed}
-                      className="routeData__routeHasSaved"
-                    />
-                    <Link to={"/create-route"}>
-                      <button className="routeData__navButtons">
-                        Plot New Route
-                      </button>
-                    </Link>
-                    <Link to={"/saved-routes"}>
-                      <button className="routeData__navButtons">
-                        My Saved Routes
-                      </button>
-                    </Link>
-                  </div>
-                )}
-                {isLoading && (
-                  <div className="routeDate__loadingDiv">
-                    <img src={logo} className="routeData__logo" />
-                    <img
-                      src={loadingsymbol}
-                      className="routeData__loadingSymbol"
-                    />
-                  </div>
-                )}
-              </div>
-            </div>
-          )}
           <section className="routeData">
-            {/* <ol className="routeData__list"> */}
-            {/* {directionsResult.routes[0].legs.map((element, index) => {
-                  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                  return (
-                    <li className="routeData__listItem" key={index}>
-                      {`Marker ${alphabet[index]} => Marker ${
-                        alphabet[index + 1]
-                      }: `}
-                      {element.duration?.text}
-                      {` Arrival Time: ${calcTime(
-                        startTime,
-                        element.duration.value
-                      )}`}
-                    </li>
-                  );
-                })} */}
-            {/* {testArray.map((element, index) => {
-                return (
-                  <li className="routeData__listItem" key={index}>
-                    {`Marker ${element.markerOrigin} => Marker ${element.markerDestination} 
-                      ${element.duration}  
-                      Arrival: ${element.arrivalTime}`}
-                  </li>
-                );
-              })} */}
             {currentTime || startTime ? (
               <div className="routeData__timingsContainer">
                 <div className="routeData__row">
@@ -403,30 +321,65 @@ export default function DirectionsData({
               </div>
             </div>
           </section>
+          {/* this && will make sure that when isPopUPOPen is true, it will render the div and all the things in it... we can change whether or not is popup is true/false by clicking on the save route or the X button as these both have the function handlePopUp (this is waaay above and basically will change the isPopUp to be true/false (the opposite of what it currently is)...*/}
+          {isPopUpOpen && (
+            <div className="routeDate__popupContainer">
+              <div className="routeData__popup">
+                <button
+                  className="routeData__popup__closingButton"
+                  onClick={handlePopUp}
+                >
+                  X
+                </button>
+                <form className="routeData__form" onSubmit={handleSubmit}>
+                  <label
+                    className="routeData__label"
+                    htmlFor="routeName"
+                  ></label>
+                  <input
+                    className="routeData__nameInput"
+                    type="text"
+                    id="routeName"
+                    name="routeName"
+                    onChange={handleInputChange}
+                    value={routeName}
+                    placeholder="Enter route name..."
+                    required
+                  />
+                  <button className="routeData__saveRouteButton">Save</button>
+                </form>
+                {isSuccess && (
+                  <div>
+                    <img
+                      src={savedRouteConfirmed}
+                      className="routeData__routeHasSaved"
+                    />
+                    <Link to={"/create-route"}>
+                      <button className="routeData__navButtons">
+                        Plot New Route
+                      </button>
+                    </Link>
+                    <Link to={"/saved-routes"}>
+                      <button className="routeData__navButtons">
+                        My Saved Routes
+                      </button>
+                    </Link>
+                  </div>
+                )}
+                {isLoading && (
+                  <div className="routeDate__loadingDiv">
+                    <img src={logo} className="routeData__logo" />
+                    <img
+                      src={loadingsymbol}
+                      className="routeData__loadingSymbol"
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
         </>
       ) : null}
-
-      {/* or render the thing we want to render */}
     </>
-    //   <div className="directions">
-    //     <h1>Directions</h1>
-    //     {directionsResult && (
-    //       <div>
-    //         <h2>{directionsResult.routes[0].summary}</h2>
-    //         <p>
-    //           {directionsResult.routes[0].legs[0].start_address.split(",")[0]} to{" "}
-    //           {directionsResult.routes[0].legs[0].end_address.split(",")[0]}
-    //         </p>
-    //         <p>Distance: {directionsResult.routes[0].legs[0].distance?.text}</p>
-    //         <p>Duration: {directionsResult.routes[0].legs[0].duration?.text}</p>
-    //         <h2>Detailed Steps</h2>
-    //         <ol>
-    //           {directionsResult.routes[0].legs[0].steps.map((step, index) => (
-    //             <li key={index}>{step.instructions.replaceAll("<b>", "")}</li>
-    //           ))}
-    //         </ol>
-    //       </div>
-    //     )}
-    //   </div>
   );
 }
